9d90ab761500b38a19d57103e42418d3c8909171
rxn233 <rxn233@student.bham.ac.uk>
2023-07-17 13:17:51 +0100

    Added navbar

---
+++ src/components/HomePage/HomePage.js
@@ -0,0 +1,12 @@
+import { Fragment } from "react";
+import NavBar from "../NavBar/NavBar";
+
+function HomePage(props) {
+  return (
+    <Fragment>
+      <NavBar />
+    </Fragment>
+  );
+}
+
+export default HomePage;
