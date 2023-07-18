31a6b9eab9d6947835fde25403fbaaec26ccd97f
rxn233 <rxn233@student.bham.ac.uk>
2023-07-18 20:44:57 +0100

    express, ejs added

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -4,7 +4,36 @@ import "./Login.css";
 function Login() {
   return (
     <div className="main-login">
-      <h2>Login</h2>
+      <form className="login-form">
+        <h1>Login</h1>
+        <div className="form-input-material">
+          <input
+            type="text"
+            name="username"
+            id="username"
+            placeholder=" "
+            autoComplete="off"
+            className="form-control-material"
+            required
+          />
+          <label for="username">Username</label>
+        </div>
+        <div className="form-input-material">
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder=" "
+            autoComplete="off"
+            className="form-control-material"
+            required
+          />
+          <label for="password">Password</label>
+        </div>
+        <button type="submit" class="btn btn-primary btn-ghost">
+          Login
+        </button>
+      </form>
     </div>
   );
 }
