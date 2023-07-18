a5d1e5d8b292d34b0bc912175ba0f2d92436b382
rxn233 <rxn233@student.bham.ac.uk>
2023-07-18 22:15:44 +0100

    Update Login.js

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,13 +1,32 @@
-import { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import "./Login.css";
 
 function Login() {
+  const [enteredUsername, setEnteredUsername] = useState("");
+  const [enteredPassword, setEnteredPassword] = useState("");
+  function nameChangeHandler(event) {
+    console.log("Name");
+    setEnteredUsername(event.target.value);
+  }
+
+  function passwordChangeHandler(event) {
+    console.log("Password changed");
+    setEnteredPassword(event.target.value);
+  }
+
+  function submitHandler(event) {
+    event.preventDefault();
+    console.log("Submitted");
+    console.log(enteredUsername, enteredPassword);
+  }
+
   return (
     <div className="main-login">
-      <form className="login-form">
+      <form onSubmit={submitHandler} className="login-form">
         <h1>Login</h1>
         <div className="form-input-material">
           <input
+            onChange={nameChangeHandler}
             type="text"
             name="username"
             id="username"
@@ -16,10 +35,11 @@ function Login() {
             className="form-control-material"
             required
           />
-          <label for="username">Username</label>
+          <label htmlFor="username">Username</label>
         </div>
         <div className="form-input-material">
           <input
+            onChange={passwordChangeHandler}
             type="password"
             name="password"
             id="password"
@@ -28,9 +48,9 @@ function Login() {
             className="form-control-material"
             required
           />
-          <label for="password">Password</label>
+          <label htmlFor="password">Password</label>
         </div>
-        <button type="submit" class="btn btn-primary btn-ghost">
+        <button type="submit" className="btn btn-primary btn-ghost">
           Login
         </button>
       </form>
