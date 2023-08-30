a687ad1d06e132563547afbebe657c159cb5650a
Rohit Nair <rxn233@student.bham.ac.uk>
2023-08-30 17:16:26 +0100

    Update MyProfile.js

--- client/src/components/MyProfile/MyProfile.js
+++ client/src/components/MyProfile/MyProfile.js
@@ -9,7 +9,15 @@ function MyProfile(props) {
   const navigate = useNavigate();
 
   const [userId, setUserId] = useState(localStorage.getItem("user_id"));
-  const [userData, setUserData] = useState({});
+  // const [userData, setUserData] = useState({});
+  // const [editedData, setEditedData] = useState({ userData });
+  const [isEditing, setIsEditing] = useState(false);
+  //Adding separate values
+  const [userName, setUserName] = useState("");
+  const [userAddress, setUserAddress] = useState("");
+  const [userContact, setUserContact] = useState("");
+  const [userLocation, setUserLocation] = useState("");
+  const [userEmail, setUserEmail] = useState("");
   // console.log("Myprofile", userId);
 
   useEffect(() => {
@@ -21,7 +29,13 @@ function MyProfile(props) {
           })
           .then(function (response) {
             console.log("Response data", response.data);
-            setUserData(response.data);
+            console.log("Response data", response.data.user_name);
+            setUserName(response.data.user_name);
+            setUserAddress(response.data.user_address);
+            setUserContact(response.data.user_contact);
+            setUserLocation(response.user_location);
+            setUserEmail(response.data.user_email);
+            // setUserData(response.data);
           })
           .catch(function (error) {
             console.log("Error", error);
@@ -33,14 +47,76 @@ function MyProfile(props) {
     navigate("/");
   }
 
+  function editHandler(event) {
+    setIsEditing(true);
+  }
+
+  function profileSaveHandler() {
+    // console.log("UserData", userData);
+    // console.log("EditedData", editedData);
+    console.log(userEmail);
+    console.log(userContact);
+    console.log(userAddress);
+    console.log(userName);
+  }
+
+  function cancelHandler() {
+    setIsEditing(false);
+  }
+
   return (
     <div>
       <h2>My profile</h2>
-      <div className="profile">
-        <h1 className="name">Username: {userData.user_name}</h1>
-        <p className="email">Email: {userData.user_email}</p>
-        <p className="address">Address: {userData.user_address}</p>
-      </div>
+      <button onClick={editHandler}>Edit the profile</button>
+      {isEditing ? (
+        <div>
+          <p>Email: {userEmail}</p>
+          <label htmlFor="user_name">Name: </label>
+          <input
+            name="user_name"
+            id="user_name"
+            value={userName}
+            onChange={(event) => {
+              setUserName(event.target.value);
+            }}
+          />
+          <label htmlFor="user_address">Address: </label>
+          <input
+            id="user_address"
+            name="user_address"
+            value={userAddress}
+            onChange={(event) => {
+              setUserAddress(event.target.value);
+            }}
+          />
+          <label htmlFor="user_contact">Contact: </label>
+          <input
+            name="user_contact"
+            id="user_contact"
+            value={userContact}
+            onChange={(event) => {
+              setUserContact(event.target.value);
+            }}
+          />
+          <label htmlFor="user_location">Location: </label>
+          <input
+            id="user_location"
+            name="user_location"
+            value={userLocation}
+            onChange={(event) => {
+              setUserLocation(event.target.value);
+            }}
+          />
+          <button onClick={profileSaveHandler}>Save</button>
+          <button onClick={cancelHandler}>Cancel</button>
+        </div>
+      ) : (
+        <div className="profile">
+          <h1 className="name">Username: {userName}</h1>
+          <p className="email">Email: {userEmail}</p>
+          <p className="address">Address: {userAddress}</p>
+        </div>
+      )}
       <button onClick={logOutHandler}>Log Out</button>
     </div>
   );
